50751f6c853fe659e79c8c8ad3f6a9c487dff41c
Gabriel Scotá <gabrielscota2015@gmail.com>
2025-05-22 00:42:17 -0300

    🛠️: Refactor decrypt and encrypt examples to remove unnecessary comments and streamline code

--- lib/example/encrypt.dart
+++ lib/example/encrypt.dart
@@ -4,22 +4,19 @@ import 'dart:io';
 import 'package:pointycastle/export.dart';
 import 'package:rsa_oaep_dart/rsa_oaep_dart.dart';
 
+// ignore_for_file: avoid_print
+
 void main() {
-  // 1. Carregar a chave pública
   final pem = File('public.pem').readAsStringSync();
   final publicKey = RSAKeyParser.parsePublicKeyFromPem(pem);
 
-  // 2. Criar instância do RSAOAEP com SHA-256
   final oaep = RSAOAEP(hash: SHA256Digest(), publicKey: publicKey);
 
-  // 3. Mensagem para criptografar
   final message = utf8.encode('Hello, Gabriel!');
 
-  // 4. Criptografar
   final ciphertext = oaep.encrypt(message);
   print('Ciphertext: ${base64.encode(ciphertext)}');
 
-  // 5. Salvar ciphertext em arquivo binário
   final file = File('ciphertext.bin');
   file.writeAsBytesSync(ciphertext);
 
